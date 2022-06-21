6dbd5b1a23631a8e232b2a201edff11945af2ee9
eso <eso@esoui-MacBookPro.local>
2022-06-21 22:41:13 +0900

    PythonProject

--- pythonProject/urls.py
+++ pythonProject/urls.py
@@ -1,8 +1,16 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 app_name = 'pythonProject'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
 ]
+
+from django.views.static import serve
+
+url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
